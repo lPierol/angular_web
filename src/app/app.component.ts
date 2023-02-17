b64589424738f26c783426aa738de93ea4f049e4
Piero Rodriguez <pierorodriiguez@gmail.com>
2023-02-17 00:14:23 -0300

    10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Curso } from './models/curso';
+import { CursoAlphaService } from './services/curso-alpha.service';
+import { CursoService } from './services/curso.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+
+  constructor(
+    private cursoService: CursoService
+  ){
+
+  }
+
+  agregarCurso(){
+    let c: Curso = {
+      nombre: 'Angular Avanzando',
+      comision: '34022',
+      fechaInicio: new Date(),
+      fechaFin: new Date(),
+      inscripcionAbierta: true,
+      profesor: {
+        nombre: 'Ulises',
+        correo: 'ulises@gmail.com',
+        fechaRegistro: new Date()
+      }
+    };
+    this.cursoService.agregarCurso(c);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FormularioReactivoComponent } from './components/formulario-reactivo/formulario-reactivo.component';
+import { ListaCursosComponent } from './components/lista-cursos/lista-cursos.component';
+import { CursoService } from './services/curso.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { TablaCursosComponent } from './components/tabla-cursos/tabla-cursos.component';
+import { CursoAlphaService } from './services/curso-alpha.service';
+import { cursos } from './services/cursos.data';
+import { env } from 'src/environment/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioReactivoComponent
+    ListaCursosComponent,
+    TablaCursosComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
